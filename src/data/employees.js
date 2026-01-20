e75b54c1206056188c1c2d47e2601f21fd48839c
AAYUSH TALREJA <amtalreja02@gmail.com>
2026-01-21 01:02:38 +0530

    Enhance UI responsiveness and improve styling in App component

--- src/data/employees.js
+++ src/data/employees.js
@@ -3,273 +3,273 @@ const employees = {
           name: "Aayush Talreja",
           title: "Senior Designer",
           email: "aayush.talreja@optimite.net",
-          photo: "../../public/employees/aayush.png"
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Aayush.png"
         },
       
         aditya: {
           name: "Aditya Tiwari",
           title: "Email Designer Intern",
           email: "aditya.tiwari@optimite.net",
-          photo: "../../public/employees/aditya.png"
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Aditya.png"
         },
       
         anamika: {
           name: "Anamika Kalwan",
           title: "Head of Content",
           email: "anamika.kalwan@optimite.net",
-          photo: "../../public/employees/anamika.png"
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Anamika.png"
         },
       
         arpit: {
           name: "Arpit Mehar",
           title: "Content Writer",
           email: "arpit.mehar@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Arpit.png"
         },
       
         ayush_agrawal: {
           name: "Ayush Agrawal",
           title: "Account Manager",
           email: "ayush.agrawal@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Ayush.png"
         },
       
         ayush_maan: {
           name: "Ayush Maan",
           title: "Head of Research",
           email: "ayush.maan@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Ayushmaan.png"
         },
       
         chhavi: {
           name: "Chhavi Singh",
           title: "Client Success Manager",
           email: "chhavi.singh@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Chhavi.png"
         },
       
         dhruvi: {
           name: "Dhruvi Vithlani",
           title: "Graphic Designer",
           email: "dhruvi.vithlani@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Dhruvi.png"
         },
       
         farhana: {
           name: "Farhana Nasreen",
           title: "Account Manager",
           email: "farhana.nasreen@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Farhana.png"
         },
       
         gaurang: {
           name: "Gaurang Nagar",
           title: "Associate - Email Marketing",
           email: "gaurang.nagar@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Gaurang.png"
         },
       
         gaurav: {
           name: "Gaurav Pandey",
           title: "Graphic Designer",
           email: "gaurav.pandey@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Gaurav.png"
         },
       
         harish_parameshwar: {
           name: "Harish Parameshwar",
           title: "Founder’s Office Strategy & Ops Lead",
           email: "harish.parameshwar@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Harish.png"
         },
       
         harsh: {
           name: "Harsh Kumar",
           title: "Head Of Operation",
           email: "harsh.kumar@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Harsh.png"
         },
       
         harshita: {
           name: "Harshita Prasad",
           title: "Graphic Designer",
           email: "harshita.prasad@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Harshita.png"
         },
       
         kanchan: {
           name: "Kanchan Yadav",
           title: "Head of Design",
           email: "kanchan.yadav@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Kanchan.png"
         },
       
         keyur: {
           name: "Keyur Rawal",
           title: "Executive Assistant",
           email: "keyur.rawal@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Keyur.png"
         },
       
         kim: {
           name: "Kim Longden",
           title: "Email Designer",
           email: "kim.longden@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Kim.png"
         },
       
         madhav: {
           name: "Madhav Yadav",
           title: "Sales and Marketing",
           email: "madhav.yadav@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Madhav.png"
         },
       
         mahima: {
           name: "Mahima Joshi",
           title: "Graphic Designer",
           email: "mahima.joshi@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Mahima.png"
         },
       
         monika: {
           name: "Monika Thapa",
           title: "Account Manager",
           email: "monika.thapa@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Monica.png"
         },
       
         natwar: {
           name: "Natwar Singh Rathore",
           title: "Graphic Designer",
           email: "natwar.rathore@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Natwar.png"
         },
       
         nishant: {
           name: "Nishant Yadav",
           title: "Co-Founder",
           email: "nishant@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Nishant.png" 
         },
       
         paridhi: {
           name: "Paridhi Rastogi",
           title: "Associate-CSM",
           email: "paridhi.rastogi@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Paridhi.png"
         },
       
         prachi: {
           name: "Prachi Yadav",
           title: "Email Designer",
           email: "prachi.yadav@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Prachi.png"
         },
       
         priyanka: {
           name: "Priyanka Nandi",
           title: "Graphic Designer",
           email: "priyanka.nandi@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Priyanka.png"
         },
       
         raph: {
           name: "Raphael Mgbeme",
           title: "Account Manager",
           email: "raphael.mgbeme@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Raphael.png"
         },
       
         ratnavi: {
           name: "Ratnavi Shetty",
           title: "Account Manager",
           email: "ratnavi.shetty@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Ratnavi.png"
         },
       
         santu: {
           name: "Santu Kumar",
           title: "Sr. Associate Email Developer",
           email: "santu.kumar@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Santu.png"
         },
       
         shaina: {
           name: "Shaina Shingari",
           title: "Email Design Intern",
           email: "shaina.shingari@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Shaina.png"
         },
       
         shrestha: {
           name: "Shrestha Ghosal",
           title: "Content Writer",
           email: "shrestha.ghosal@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Shrestha.png"
         },
       
         shyam: {
           name: "Shyam Agarwal",
           title: "Head of Operations",
           email: "shyam.agarwal@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Shyam.png"
         },
       
         simran: {
           name: "Simran Verma",
           title: "HR",
           email: "simran.verma@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Simran.png"
         },
       
         sumit: {
           name: "Sumit Verma",
           title: "Sr. Associate Email Developer",
           email: "sumit.verma@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Sumit.png"
         },
       
         suraj: {
           name: "Suraj Nishad",
           title: "Web Developer",
           email: "suraj.nishad@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Suraj.png"
         },
       
         sushant: {
           name: "Sushant Yadav",
           title: "Co-Founder",
           email: "sushant@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Sushant.png"
         },
       
         tanvi: {
           name: "Tanvi Sundaram",
           title: "Creative Content Producer",
           email: "tanvi.sundaram@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Tanvi.png"
         },
       
         upasana: {
           name: "Upasana Barua",
           title: "Graphic Designer",
           email: "upasana.barua@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Upasana.png"
         },
       
         vishal: {
           name: "Vishal Kumar",
           title: "",
           email: "vishal.kumar@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Vishal.png"
         },
       
         yashvi: {
           name: "Yashvi Sanghvi",
           title: "Chief of Staff",
           email: "yashvi.sanghvi@optimite.net",
-          photo: ""
+          photo: "https://raw.githubusercontent.com/aayu5hgit/ESG/refs/heads/main/public/employees/Yashvi.png"
         }
       };
       
